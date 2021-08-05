b65e29b71ec31375e6affbb36b279994fdc6ca6a
Alireza Mohamadzadeh <alirezamz.org@gmail.com>
2021-08-05 14:16:13 +0430

    setup Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home, Game } from "./pages";
 
 const App = () => {
-  return <div className="text-indigo-500 text-3xl">App</div>;
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/:id" component={Game} />
+        </Switch>
+      </Router>
+    </>
+  );
 };
 
 export default App;
